8611a7ede637b81a055088b262a5382335a7a2c9
Max Eshleman <max@render.com>
2024-10-08 15:36:39 -0600

    configure scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
 const path    = require("path")
 const webpack = require("webpack")
 
+// Extracts CSS into .css file
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+// in this example, entry.custom will create a corresponding empty custom.js file
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: [
+      './app/assets/stylesheets/application.scss',
+    ],
+  },
+  module: {
+    rules: [
+      // Add CSS/SASS/SCSS rule with loaders
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    // Add additional file types
+    extensions: ['.js', '.jsx', '.scss', '.css'],
   },
   output: {
     filename: "[name].js",
@@ -14,8 +35,10 @@ module.exports = {
     path: path.resolve(__dirname, "app/assets/builds"),
   },
   plugins: [
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
   ]
 }
